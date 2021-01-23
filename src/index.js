ab93ccb5c0bb5fe455bf4a2499c87d5ea6a89d5c
benfanderson <benfanderson81@gmail.com>
2021-01-23 18:20:43 +0800

    Removed title variable from Index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-const title = 'Hot Dog';
-
 ReactDOM.render(
-  <App title={title} />,
+  <App />,
   document.getElementById('root'),
 );
 
